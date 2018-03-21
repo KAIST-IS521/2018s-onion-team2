0cb9f942bb9907b86fb74dbdf90e4024603e996e
Taek Jin Lee <hexife@kaist.ac.kr>
2018-03-22 00:55:49 +0900

    Making templete

---
+++ client/message.cc
@@ -0,0 +1,82 @@
+#include <string>
+#include "message.hh"
+using namespace std;
+
+// message::message
+message::message(){
+
+}
+
+message::~message(){
+
+}
+
+// message::setMessage
+// Description - message에 값을 할당하는 함수
+// Return - true = 성공, false = 실패
+bool message::setMessage(){
+
+}
+
+// message::getContents
+// Description - message 객체에서 Chatting Contents를 가져옴
+// Return - message::Content
+String message::getContents(){
+
+}
+
+// message::getGithubID
+// Description - message 객체에서 GithubID 를 가져옴
+// Return - message::getGithubID 값
+String message::getGithubID(){
+
+}
+
+// message::getTimedtamp
+// Description - message 객체에서 Timestamp를 가져옴
+// Return - message::timestamp 값
+long message::getTimestamp(){
+}
+
+// message::getOneTimeKey()
+// Description - message 객체에서 OneTimeKey를 가져옴
+// Return - message::OneTimeKey 값
+BYTE* message::getOneTimeKey(){
+
+}
+
+// message::isWritable()
+// Description - message 객체의 write가능 여부를 보여줌
+// Return - message::writeable 값
+bool message::isWritable(){
+}
+
+//////////////////// For Message Send ////////////////////
+
+// message::setTimestamp
+// Description - Writable Flag가 켜져있을 때 Timestamp 값을 message 객체에 기록
+// Return - true = 성공, false = 실패
+bool message::setTimestamp(long timestamp){
+}
+
+// message::setGithubID
+// Description - Writable Flag가 켜져있을 때 GithubID를 message 객체에 기록
+// Return - true = 성공, false = 실패
+bool message::setGithubID(String githubID){
+
+}
+
+// message::setOneTimeKey
+// Description - Writeable Flag가 켜져있을 때 OneTimeKey를 message 객체에 기록
+// Return - true = 성공, false = 실패
+bool message::setOneTimeKey(BYTE* OneTimeKey){
+
+}
+
+// message::setContents
+// Description - Writable Flag가 켜져있을 때 GithubID를 message 객체에 기록
+// Return - true = 성공, false = 실패
+bool message::setContents(String content){
+
+}
+
