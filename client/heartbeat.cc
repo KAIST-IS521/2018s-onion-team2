85297dfced2544b09cc03edd4e4e585f322d0f45
hongsh93 <hongsh93@kaist.ac.kr>
2018-03-23 14:48:29 +0900

    Add heartbeat

---
+++ client/heartbeat.cc
@@ -0,0 +1,24 @@
+#include "heartbeat.hh"
+using namespace std;
+
+
+// heartbeat::recvHeartbeat
+// 서버로부터 heartbeat 메시지를 받았는지를 확인하는 함수
+// return - 받았으면 true, 아니면 false
+bool heartbeat::recvHeartbeat()
+{
+}
+
+// heartbeat::ParseMessage
+// 서버로부터 받은 heartbeat 메시지를 parsing하는 함수
+// return -
+String ParseMessage()
+{
+}
+
+// heartbeat::SendHeartbeat
+// 서버에게 heartbeat 메시지를 전달하는 함수
+// return - 전달했으면 true, 아니면 false
+bool heartbeat::SendHeartbeat()
+{
+}
