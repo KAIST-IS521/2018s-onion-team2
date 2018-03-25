43fe364b6fbb48fa75790e6b0e66d857d18603b3
Su Young Lee <sevendays37@kaist.ac.kr>
2018-03-25 23:52:54 +0900

    Changing indentations from tabs to spaces

--- client/ui.cc
+++ client/ui.cc
@@ -5,31 +5,31 @@
 using namespace std;
 
 userInfo ui::login(){
-	string GithubId, PublicKeyID, IP, Passphrase;
-	clearScreen();
-	cout << "Please enter your Github ID: ";
-	cin >> GithubId;
-	cout << "Please enter your public key ID: ";
-	cin >> PublicKeyID;
-	cout << "Please enter passphrase: ";
-	cin >> Passphrase;
+  string GithubId, PublicKeyID, IP, Passphrase;
+  clearScreen();
+  cout << "Please enter your Github ID: ";
+  cin >> GithubId;
+  cout << "Please enter your public key ID: ";
+  cin >> PublicKeyID;
+  cout << "Please enter passphrase: ";
+  cin >> Passphrase;
 
-	// We may need to change the code to automatically detect user's ip address
-	cout << "Please enter your IP address: ";
-	cin >> IP;
+  // We may need to change the code to automatically detect user's ip address
+  cout << "Please enter your IP address: ";
+  cin >> IP;
 
-	return userInfo(GithubId, PublicKeyID, IP, Passphrase);
+  return userInfo(GithubId, PublicKeyID, IP, Passphrase);
 }
 
 void ui::clearScreen()
 {
-	cout << "\e[1;1H\e[2J";
+  cout << "\e[1;1H\e[2J";
 }
 
 // ui::printBanner
 // Description - 프로그램 시작시 Banner를 띄워줌
 // return - None
 void ui::printBanner(userInfo user){
-	clearScreen();
-	cout << "Welcome to Onion Messanger, " + user.getGithubID() + "!!!" << endl;
+  clearScreen();
+  cout << "Welcome to Onion Messanger, " + user.getGithubID() + "!!!" << endl;
 }
