972789a2f688b0cd34a92fb8b217512ce0bfafd9
Taek Jin Lee <hexife@kaist.ac.kr>
2018-03-29 02:07:26 +0900

    Test complete: heartbeat recv/send/listener

--- client/heartbeat.cc
+++ client/heartbeat.cc
@@ -1,27 +1,26 @@
 #include "heartbeat.hh"
-
-
-
+using namespace std;
 // heartbeat::heartbeat
 // Description - heartbeat 생성자
 // return - None
 heartbeat::heartbeat(char* _OneTimeKey, time_t _timestamp){
-  this.OneTimeKey = _OneTimeKey;
-  this.timestamp = _timestamp;
+  this->OneTimeKey = _OneTimeKey;
+  this->timestamp = _timestamp;
 }
+
 
 // heartbeat::getOneTimeKey
 // Description - heartbeat 객체에서 OntTimeKey를 가져옴
 // return - Null(실패), char*(OneTimeKey stream)
 char* heartbeat::getOneTimeKey(){
-  return this.OneTimeKey;
+  return this->OneTimeKey;
 }
 
 // heartbeat::getTimestamp
 // Description - heartbeat 객체에서 Timestamp를 가져옴
 // return - Null(실패), time_t(Timestamp)
 time_t heartbeat::getTimestamp(){
-  return this.timestamp;
+  return this->timestamp;
 }
 
 
@@ -30,10 +29,10 @@ time_t heartbeat::getTimestamp(){
 // return - True(성공), False(실패)
 bool heartbeat::setOntTimeKey(char* _OneTimeKey){
   try{
-    this.OneTimeKey = _OneTimeKey;
+    this->OneTimeKey = _OneTimeKey;
     return true;
   }
-  catch(){
+  catch(int exception){
     return false;
   }
 } 
@@ -44,10 +43,10 @@ bool heartbeat::setOntTimeKey(char* _OneTimeKey){
 // return - True(성공), False(실패)
 bool heartbeat::setTimestamp(time_t _timestamp){
   try{
-    this.timestamp = timestamp;
+    this->timestamp = _timestamp;
     return true;
   }
-  catch(){
+  catch(int exception){
     return false;
   }
 }
@@ -57,34 +56,46 @@ bool heartbeat::setTimestamp(time_t _timestamp){
 // return - Null(실패), char*
 char* hbd::recvHeartbeat()
 {
-  char buffer[10];
+  int socketFd=-1;
+  char* buffer=NULL;
   try{
-    int socketFd = socket(AF_INET, SOCK_DGRAM,0);
-    if(-1 == sock)
+    socketFd = socket(AF_INET, SOCK_DGRAM,0);
+    if(-1 == socketFd)
     { 
       return NULL;
     }
-  
     struct sockaddr_in server_info;
     memset(&server_info,0,sizeof(server_info)); // initalize server_info to 0
     server_info.sin_family=AF_INET;
     server_info.sin_port = htons(HB_PORT);              // discuss about it ### PORT ###
-    server_info.sin_adr.s_addr=inet_addr("127.0.0.1"); // discuss about it ### SERVER IP ###
+    server_info.sin_addr.s_addr=inet_addr("127.0.0.1"); // discuss about it ### SERVER IP ###
 
-    if(-1 == bind(socketFd,(struct sockaddr*)&server_addr,sizeof(server_addr))){
+    if(-1 == bind(socketFd,(struct sockaddr*)&server_info,sizeof(struct sockaddr))){
+      close(socketFd);
       return NULL;
     }
-    recvfrom(socketFd, &buffer, 10,0,(struct sockaddr*)&server_info,sizeof(server_info));
+
+    socklen_t server_len = sizeof(struct sockaddr);
+    buffer = new char[10];
+    int recv_len = recvfrom(socketFd, buffer, 10,0,(struct sockaddr*)&server_info,&server_len);
     if(buffer[0]!=0x04){
+      close(socketFd);
+      delete buffer;
       return NULL; // check check check
     }
-    close(socketFd);
   }
-  catch(){
+  catch(int exception){
+    if (socketFd != -1){
+      close(socketFd);
+    }
+    if (buffer != NULL){
+      delete buffer;
+    }
     return NULL;
   }
-  return buffer;
 
+  close(socketFd);
+  return buffer;
 }
 
 // hbd::heartbeatListener
@@ -92,27 +103,26 @@ char* hbd::recvHeartbeat()
 // return - None
 void hbd::heartbeatListener()
 {
+  char* gotData = NULL;
+  heartbeat* parsedData = NULL;
   while(true){
     try{
-      char* gotData = recvHeartbeat();
+      gotData = hbd::recvHeartbeat();
       if(gotData!=NULL){
-        heartbeat parsedData = new heartbeat(gotData);
-        parsedData.setTimestamp(timestamp::getTimestampNow());
-        char* packedData = parser::packHeartBeat(&parsedData);
-        
-        if(packedData!=NULL){
-        sendHeartbeat(packedData);
-        
+     /* 
+        1. parsing gotData to heartbeat object
+        2. set new timestamp
+        3. pack to char* stream
+        4. send back to server
+    */
         }
         else{
-          throw;
+          cout << "Wrong Flag Occured" << endl;
         }        
       }
+    catch(int exception){
+      cout << "Error Occured!" << endl;
     }
-    catch(){
-
-    }
-    delete parsedData;
   }
 }
 
@@ -121,25 +131,27 @@ void hbd::heartbeatListener()
 // return - 전달했으면 true, 아니면 false
 bool hbd::sendHeartbeat(char* send_source, int send_size)
 {
-  try{
-    int socketFd = socket(AF_INET, SOCK_DGRAM,0);
-    if(-1 == sock)
-    {
-      return false;
-    }
-  
-    struct sockaddr_in server_info;
-    memset(&server_info,0,sizeof(server_info)); // initalize server_info to 0
-    server_info.sin_family=AF_INET;
-    server_info.sin_port = htons(HB_PORT);              // discuss about it ### PORT ###
-    server_info.sin_addr.s_addr=inet_addr("127.0.0.1"); // discuss about it ### SERVER IP ###
-  
-    sendto(socketFd, send_source, send_size+1,0,(struct sockaddr*)&server_info,sizeof(server_info));
-  }
-  catch(){
-    close(socketFd);
+  char* buf;
+  int recvFd;
+  int sockFd = socket(AF_INET, SOCK_DGRAM,0);
+
+  if (sockFd < 0){
     return false;
   }
-  close(socketFd);
+  struct sockaddr_in sock_info, conn_info;
+  int conn_len = sizeof(conn_info);
+  bzero((char*)&sock_info, sizeof(sock_info));
+  sock_info.sin_family = AF_INET;
+  sock_info.sin_addr.s_addr = inet_addr("127.0.0.1");
+  sock_info.sin_port = htons(HB_PORT);
+
+  if (0 > connect(sockFd,(struct sockaddr *) &sock_info,sizeof(sock_info))){
+    return false;
+  }
+
+  if (0 >  send(sockFd,send_source,send_size,0)){
+    return false;
+  }
+  close(sockFd);
   return true;
 }
