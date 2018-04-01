4170d087a9cb87faa4a431fcca0ca6324d895d34
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-02 07:20:17 +0900

    Modification to use userInfo in threads

--- source_code/client_main.cc
+++ source_code/client_main.cc
@@ -9,31 +9,27 @@ using namespace std;
 
 int main()
 {
-  
-  userInfo user = ui::login();
+  user = ui::login();
   
   struct tmd::arg_main* msg_arguments = new struct tmd::arg_main();
-  tmd::msg_args(user, msg_arguments);
+  tmd::msg_args(msg_arguments);
 
-  // struct tmd::arg_main* hearbeat_arguments = new struct tmd::arg_main();
-  // hbd::heartbeat_args(user, hearbeat_arguments);
+  struct tmd::arg_main* hearbeat_arguments = new struct tmd::arg_main();
+  hbd::heartbeat_args(hearbeat_arguments);
 
   struct tmd::arg_data* list_update_arguments = new struct tmd::arg_data();
-  parser::packListUpdate(SIGN_IN, user, list_update_arguments);
+  parser::packListUpdate(SIGN_IN, list_update_arguments);
   
-  pthread_t th_msg, th_heartbeat, th_list_update;
+  pthread_t th_msg, th_heartbeat, th_list_update, th_input;
   pthread_create(&th_msg, NULL, tmd::tmdReceiverMain, (void*)msg_arguments);
-  // pthread_create(&th_heartbeat, NULL, tmd::tmdReceiverMain, (void*)hearbeat_arguments);
+  pthread_create(&th_heartbeat, NULL, tmd::tmdReceiverMain, (void*)hearbeat_arguments);
   pthread_create(&th_list_update, NULL, tmd::tmdSender, (void*)list_update_arguments);
+  pthread_create(&th_input, NULL, ui::userInput, NULL);
 
   pthread_join(th_msg, NULL);
-  // pthread_join(th_heartbeat, NULL);
+  pthread_join(th_heartbeat, NULL);
   pthread_join(th_list_update, NULL);
+  pthread_join(th_input, NULL);
 
-  // Send an info that user has logged-in to the server
-
-  // Retrieve client list from the Server
-
-  // Create a thread for the listening
   return 0;
 }
