250af7a977f54e5df20e6956800507690b8480d6
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-02 06:48:49 +0900

    Adding a makefile

--- source_code/compile.sh
+++
@@ -1,3 +0,0 @@
-g++ client_main.cc util.cc ui.cc userInfo.cc nodelist.cc transmission.cc heartbeat.cc timestamp.cc parser.cc gpg.cc -o client -lpthread -std=c++11
-g++ server_main.cc list_check.cc util.cc nodelist.cc parser.cc timestamp.cc gpg.cc transmission.cc userInfo.cc -o server -lpthread -std=c++11
-
