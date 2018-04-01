9ad183e2b4d18b8ff60225576b9025a68e5a6852
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-02 04:50:57 +0900

    Reflecting changes from util

--- source_code/heartbeat.cc
+++ source_code/heartbeat.cc
@@ -10,11 +10,11 @@ char* heartbeat::getOneTimeKey(){
 }
 
 void heartbeat::setHeartBeat(char* data, char* response){
-  // char time_stamp[TIME_T_SIZE];
-  // response[0] = data[0];
-  // memcpy(response+1, this->getOneTimeKey(), 4);
-  // util::time_t2byte(timestamp::getTimestampNow(), time_stamp);
-  // memcpy(response+5, time_stamp, 4);
+  response[0] = data[0];
+  memcpy(response+1, this->getOneTimeKey(), 4);
+  char* time_stamp = timestamp::timestamp2byte(timestamp::getTimestampNow());
+  memcpy(response+5, time_stamp, 4);
+  delete time_stamp;
 }
 
 void* hbd::recvHeartbeat(void* args)
@@ -53,40 +53,9 @@ void* hbd::recvHeartbeat(void* args)
   }
 }
 
-// // hbd::SendHeartbeat
-// // Description - 서버에게 heartbeat 메시지를 전달하는 함수
-// // return - 전달했으면 true, 아니면 false
-// bool hbd::sendHeartbeat(char* send_source, int send_size)
-// {
-//   char* buf;
-//   int recvFd;
-//   int sockFd = socket(AF_INET, SOCK_DGRAM,0);
-
-//   if (sockFd < 0){
-//     return false;
-//   }
-//   struct sockaddr_in sock_info, conn_info;
-//   int conn_len = sizeof(conn_info);
-//   bzero((char*)&sock_info, sizeof(sock_info));
-//   sock_info.sin_family = AF_INET;
-//   sock_info.sin_addr.s_addr = HB_SERVER_ADDR;
-//   sock_info.sin_port = htons(HB_PORT);
-
-//   if (0 > connect(sockFd,(struct sockaddr *) &sock_info,sizeof(sock_info))){
-//     return false;
-//   }
-
-//   if (0 >  send(sockFd,send_source,send_size,0)){
-//     return false;
-//   }
-//   close(sockFd);
-//   return true;
-// }
-
-void hbd::heartbeat_args(userInfo user, struct tmd::arg_main* arguments){
+void hbd::heartbeat_args(struct tmd::arg_main* arguments){
   arguments->port = HB_PORT;
   arguments->protocol = IPPROTO_UDP;
   arguments->type = SOCK_DGRAM;
-  arguments->user = user;
   arguments->func = hbd::recvHeartbeat;
 }
