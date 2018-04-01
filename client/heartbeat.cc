680d485a277a71f7b3366c5edec235c7bd48e41d
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-01 12:40:10 +0900

    Fixing a bug in hearbeat.cc

--- client/heartbeat.cc
+++ client/heartbeat.cc
@@ -39,38 +39,38 @@ void* hbd::recvHeartbeat(void* args)
   close(recvFd);
 }
 
-// hbd::SendHeartbeat
-// Description - 서버에게 heartbeat 메시지를 전달하는 함수
-// return - 전달했으면 true, 아니면 false
-bool hbd::sendHeartbeat(char* send_source, int send_size)
-{
-  char* buf;
-  int recvFd;
-  int sockFd = socket(AF_INET, SOCK_DGRAM,0);
+// // hbd::SendHeartbeat
+// // Description - 서버에게 heartbeat 메시지를 전달하는 함수
+// // return - 전달했으면 true, 아니면 false
+// bool hbd::sendHeartbeat(char* send_source, int send_size)
+// {
+//   char* buf;
+//   int recvFd;
+//   int sockFd = socket(AF_INET, SOCK_DGRAM,0);
 
-  if (sockFd < 0){
-    return false;
-  }
-  struct sockaddr_in sock_info, conn_info;
-  int conn_len = sizeof(conn_info);
-  bzero((char*)&sock_info, sizeof(sock_info));
-  sock_info.sin_family = AF_INET;
-  sock_info.sin_addr.s_addr = HB_SERVER_ADDR;
-  sock_info.sin_port = htons(HB_PORT);
+//   if (sockFd < 0){
+//     return false;
+//   }
+//   struct sockaddr_in sock_info, conn_info;
+//   int conn_len = sizeof(conn_info);
+//   bzero((char*)&sock_info, sizeof(sock_info));
+//   sock_info.sin_family = AF_INET;
+//   sock_info.sin_addr.s_addr = HB_SERVER_ADDR;
+//   sock_info.sin_port = htons(HB_PORT);
 
-  if (0 > connect(sockFd,(struct sockaddr *) &sock_info,sizeof(sock_info))){
-    return false;
-  }
+//   if (0 > connect(sockFd,(struct sockaddr *) &sock_info,sizeof(sock_info))){
+//     return false;
+//   }
 
-  if (0 >  send(sockFd,send_source,send_size,0)){
-    return false;
-  }
-  close(sockFd);
-  return true;
-}
+//   if (0 >  send(sockFd,send_source,send_size,0)){
+//     return false;
+//   }
+//   close(sockFd);
+//   return true;
+// }
 
 void hbd::heartbeat_args(userInfo user, struct arg_main* arguments){
-  arguments->port = MESSAGE_PORT;
+  arguments->port = HB_PORT;
   arguments->protocol = IPPROTO_UDP;
   arguments->type = SOCK_DGRAM;
   arguments->user = user;
