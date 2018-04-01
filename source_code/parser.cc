8debb9591bc9c5e0503657c8b9720344d4de3da4
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-02 07:24:28 +0900

    Uncommenting functions

--- source_code/parser.cc
+++ source_code/parser.cc
@@ -2,38 +2,38 @@
 using namespace std;
 
 
-// // parser::messageParser
-// // Description - 0x01(평문) char stream에 대한 Parsing을 실시하고 message 객체 형태로 반환
-// // Return - Null(실패), message*
-// message* parser::messageParser(char* stream){
-//   message* temp;
-//   if (stream[0] != '\x01'){
-//     cout << "Wrong Parser" << endl;
-//     return NULL;
-//   }
+// parser::messageParser
+// Description - 0x01(평문) char stream에 대한 Parsing을 실시하고 message 객체 형태로 반환
+// Return - Null(실패), message*
+message* parser::messageParser(char* stream){
+  message* temp;
+  if (stream[0] != '\x01'){
+    cout << "Wrong Parser" << endl;
+    return NULL;
+  }
 
-//   char* tmpOTK = new char[4];
-//   memcpy(tmpOTK, stream+5, 4);
+  char* tmpOTK = new char[4];
+  memcpy(tmpOTK, stream+5, 4);
 
-//   char* tmpTimestamp = new char[4];
-//   memcpy(tmpTimestamp, stream+9, 4);
-//   time_t tmpTstamp = timestamp::byte2timestamp(tmpTimestamp);
+  char* tmpTimestamp = new char[4];
+  memcpy(tmpTimestamp, stream+9, 4);
+  time_t tmpTstamp = timestamp::byte2timestamp(tmpTimestamp);
 
-//   int* tmpGithubIDLen = (int*)(stream+13);
-//   char* tmpGithubID = new char[*tmpGithubIDLen];
-//   memcpy(tmpGithubID,stream+17,*tmpGithubIDLen);
-//   string ID(tmpGithubID);
+  int* tmpGithubIDLen = (int*)(stream+13);
+  char* tmpGithubID = new char[*tmpGithubIDLen];
+  memcpy(tmpGithubID,stream+17,*tmpGithubIDLen);
+  string ID(tmpGithubID);
 
-//   int* tmpMsgLen = (int*)(stream+17+*tmpGithubIDLen);
-//   char* tmpMsg = new char[*tmpMsgLen];
-//   memcpy(tmpMsg,stream+21+*tmpGithubIDLen,*tmpMsgLen);
-//   string MSG(tmpMsg);
-//   temp = new message(MSG,ID,tmpOTK,tmpTstamp);
-//   delete tmpTimestamp;
-//   delete tmpGithubID;
-//   delete tmpMsg;
-//   return temp; 
-// }
+  int* tmpMsgLen = (int*)(stream+17+*tmpGithubIDLen);
+  char* tmpMsg = new char[*tmpMsgLen];
+  memcpy(tmpMsg,stream+21+*tmpGithubIDLen,*tmpMsgLen);
+  string MSG(tmpMsg);
+  temp = new message(MSG,ID,tmpOTK,tmpTstamp);
+  delete tmpTimestamp;
+  delete tmpGithubID;
+  delete tmpMsg;
+  return temp; 
+}
 
 // parser::nodeParser
 // Description - 0x02(리스트) char stream에 대한 Parsing을 실시하고 node 객체 형태로 반환
@@ -88,35 +88,35 @@ node* parser::nodeParser(char* stream){
 //   return temp;
 // }
 
-// // parser::encMessageParser
-// // Description - 0x00(Encrypted Message) char stream에 대한 Parser을 실시하고 encMessage 객체 형태로 반환
-// // Return - Null(실패), encMessage*
-// encMessage* parser::encMessageParser(char* stream){
-//   encMessage* temp = new encMessage();
-//   if (stream[0] != '\x00'){
-//     cout << "Wrong Parser" << endl;
-//     return NULL;
-//   }
+// parser::encMessageParser
+// Description - 0x00(Encrypted Message) char stream에 대한 Parser을 실시하고 encMessage 객체 형태로 반환
+// Return - Null(실패), encMessage*
+encMessage* parser::encMessageParser(char* stream){
+  encMessage* temp = new encMessage();
+  if (stream[0] != '\x00'){
+    cout << "Wrong Parser" << endl;
+    return NULL;
+  }
 
-//   unsigned char* tmpIP = new unsigned char[4];
-//   memcpy(tmpIP,stream+1,4);
-//   if (!temp->setNextIP(util::byte2ip(tmpIP))){           // char to IP String
-//     cout << "Wrong Next IP" << endl;
-//     return NULL;
-//   }
-//   delete tmpIP;
+  unsigned char* tmpIP = new unsigned char[4];
+  memcpy(tmpIP,stream+1,4);
+  if (!temp->setNextIP(util::byte2ip(tmpIP))){           // char to IP String
+    cout << "Wrong Next IP" << endl;
+    return NULL;
+  }
+  delete tmpIP;
 
-//   unsigned int* enc_data_length = (unsigned int*)(stream+5);
-//   char* enc_data_char = new char[*enc_data_length];
-//   memcpy(enc_data_char, stream+9, *enc_data_length);
-//   string tmp_str(enc_data_char);
-//   if (!temp->setEncData(tmp_str)){
-//     cout << "Wrong Encrypted Data" << endl;
-//     return NULL;
-//   }
-//   delete enc_data_char;
-//   return temp;
-// }
+  unsigned int* enc_data_length = (unsigned int*)(stream+5);
+  char* enc_data_char = new char[*enc_data_length];
+  memcpy(enc_data_char, stream+9, *enc_data_length);
+  string tmp_str(enc_data_char);
+  if (!temp->setEncData(tmp_str)){
+    cout << "Wrong Encrypted Data" << endl;
+    return NULL;
+  }
+  delete enc_data_char;
+  return temp;
+}
 
 // // parser::packEncMessage
 // // Description - encMessage의 요소들을 송신 규격에 맞게 packing 하여 char*으로 반환
@@ -199,24 +199,25 @@ int parser::packNode(char* stream, node* src, char mode){
   return 22+GithubIDSize;
 }
 
-// // parser::packHeartBeat
-// // Description - heartbeat의 요소들을 송신 규격에 맞게 packing하여 char*로 반환
-// // Return - Null(실패), char(Packing 된 BYTE stream)
-// int parser::packHeartBeat(char* stream, heartbeat* src){
-//   if(stream==NULL){
-//     return 0;
-//   }
-//   try{
-//     stream[0] = '\x04'; 
-//     memcpy(stream+1, src->heartbeat::getOneTimeKey(),4);
-//     memcpy(stream+5, timestamp::timestamp2byte(src->heartbeat::getTimestamp()),4);
-//   }
-//   catch(int exception){
-//     delete stream;
-//     return 0;
-//   }
-//   return 9;
-// }
+// parser::packHeartBeat
+// Description - heartbeat의 요소들을 송신 규격에 맞게 packing하여 char*로 반환
+// Return - Null(실패), char(Packing 된 BYTE stream)
+int parser::packHeartBeat(char* stream, char* OTK){
+  if(stream==NULL){
+    return 0;
+  }
+  try{
+    stream[0] = '\x04'; 
+    memcpy(stream+1, OTK,4);
+    char* time_stamp = timestamp::timestamp2byte(timestamp::getTimestampNow());
+    memcpy(stream+5, time_stamp, 4);
+    delete time_stamp;
+  }
+  catch(int exception){
+    return 0;
+  }
+  return 9;
+}
 
 // int parser::getMessagePackLen(message* src){
 //   return 21+(src->message::getGithubID().size())+(src->message::getContents().size());
@@ -234,7 +235,7 @@ int parser::packNode(char* stream, node* src, char mode){
 //   return 9;
 // }
 
-void parser::packListUpdate(char mode, userInfo user, struct tmd::arg_data* args){
+void parser::packListUpdate(char mode, struct tmd::arg_data* args){
   args->length = 10 + TIME_T_SIZE + IP_SIZE + INT_SIZE + user.getGithubID().length();
   args->data = new char[args->length];
   args->data[0] = '\x02';
