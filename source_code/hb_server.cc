214497bae092bdefc211d5fd46bd46e278072b42
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-02 07:18:19 +0900

    Modification to test the code

--- source_code/hb_server.cc
+++ source_code/hb_server.cc
@@ -1,122 +1,163 @@
-#include "hbchecker.hh"
+#include "hb_server.hh"
+#include "parser.hh"
+#include "gpg.hh"
+#include <unistd.h>
 
 // hbchecker::setOneTimeKey
 // Description - 생성된 OneTimeKey를 할당
 // return - 없음
-void hbchecker::setOneTimeKey() {
+void hbchecker::setOneTimeKey(char* OTK) {
 	std::ifstream urand("/dev/urandom");
-  urand.read(this->OneTimeKey, sizeof(OTK_SIZE));
+  urand.read(OTK, 4);
   urand.close();
 }
 
-// hbchecker::setTimestamp
-// Description - 생성된 Timestamp를 할당
-// return - 없음
-void hbchecker::setTimestamp(time_t _Timestamp) {
-	timestamp = _Timestamp;
-}
+void* hbchecker::hbCheckerMain(void* args){
+	while(true){
+		// pthread_mutex_lock(&m_node_list);
+		// vector<string>* id_list = node_list.getGithubIDList();
+		// pthread_mutex_unlock(&m_node_list);
 
+	 //  for(std::vector<string>::iterator it = id_list->begin() ; it != id_list->end(); ++it){
+	 //    pthread_mutex_lock(&m_node_list);
+	 //    node* _node = node_list.searchNode(*it, 0);
+	 //    pthread_mutex_unlock(&m_node_list);
+	 //    if(_node != NULL){
+	 //    	char OTK[4];
+	 //    	char stream[HB_LEN];
+	 //    	hbchecker::setOneTimeKey(OTK);
+	 //    	parser::packHeartBeat(stream, OTK);
 
-// hbchecker::hbchecker
-// Description - Server에서 Heartbeat를 위한 초기 소켓 할당
-// return - 없음 
-hbchecker::hbchecker(char* _OneTimeKey, time_t _Timestamp)
-{
-	int err;
+	 //    }
 
-	try{
-		serv_fd = socket(AF_INET, SOCK_DGRAM, 0);
-		if (serv_fd < 0)
-			throw 1;
+	 //    char stream[MAX_LEN];
+	 //    int length = parser::packNode(stream, _node, SIGN_IN);
+	 //    // char* encStream =  gpg::encBytestream(stream, &pubKeyID, length);
 
-		memset( &serv_addr, 0, sizeof(serv_addr));
+	 //    char* encStream = gpg::encBytestream(stream, &pubKeyID);
+	 //    struct tmd::arg_data* list_update_arguments = new struct tmd::arg_data();
+	 //    tmd::data_args(_node->getIP(), encStream, list_update_arguments);
+	 //    delete encStream;
 
-		serv_addr.sin_family		= AF_INET;
-		serv_addr.sin_port 		= htons(HB_PORT);
-		serv_addr.sin_addr.s_addr	= htonl(INADDR_ANY);
+	 //    pthread_t th_list_update;
+	 //    pthread_create(&th_list_update, NULL, tmd::tmdSender, (void*)list_update_arguments);
+	    
+	 //  }
+	 //  delete id_list;
+
+		sleep(5);
+	}
 	
-		if (bind(serv_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-			throw 3;
-
-		cout << "[=] HB function Activated" << endl;
-
-	} 
-	catch(int except){
-		cout << (except == 1? "Sock":"Bind")  << "erorr occured!" << endl;
-		return;
-	}
 }
 
-// hbchecker::getOneTimeKey
-// Description - OneTimeKey를 출력
-// return - (char*)OneTimeKey
-char* hbchecker::getOneTimeKey() {
-	return OneTimeKey;
-}
-
-// hbchecker::getTimestamp
-// Description - timestamp
-// return - (time_t)timestamp
-time_t hbchecker::getTimestamp() {
-	return timestamp;
-}
+// // hbchecker::setTimestamp
+// // Description - 생성된 Timestamp를 할당
+// // return - 없음
+// void hbchecker::setTimestamp(time_t _Timestamp) {
+// 	timestamp = _Timestamp;
+// }
 
 
-// hbchecker::recvHB
-// Description - Heartbeat 수신부 생성
-// return - True or False
-bool hbchecker::recvHB()
-{
-	if (serv_fd < 1)
-		throw false;
-	try {
-		sockaddr_in send_addr;
-		int msglen;
-		int send_addrlen;
-	        int recv_err;
-		char buf[MAXSENDBUFSIZE]; 
+// // hbchecker::hbchecker
+// // Description - Server에서 Heartbeat를 위한 초기 소켓 할당
+// // return - 없음 
+// hbchecker::hbchecker(char* _OneTimeKey, time_t _Timestamp)
+// {
+// 	int err;
+
+// 	try{
+// 		serv_fd = socket(AF_INET, SOCK_DGRAM, 0);
+// 		if (serv_fd < 0)
+// 			throw 1;
+
+// 		memset( &serv_addr, 0, sizeof(serv_addr));
+
+// 		serv_addr.sin_family		= AF_INET;
+// 		serv_addr.sin_port 		= htons(HB_PORT);
+// 		serv_addr.sin_addr.s_addr	= htonl(INADDR_ANY);
 	
-		while(true) {
-			send_addrlen = sizeof(send_addr);
-			msglen = recvfrom(serv_fd, buf, MAXRECEIVEBUFSIZE, 0, (struct sockaddr*)&send_addr, (socklen_t*)&send_addrlen);
-			buf[msglen] = '\0';
-			cout << "[UDP/" << inet_ntoa(send_addr.sin_addr) << ":" << ntohs(send_addr.sin_port) << "]" << endl;
-		}
-	} catch(bool except) {
-		cout << "Initialization of the socket is needed!" << endl;
-		close(serv_fd);
-		return false;
-	}
-	close(serv_fd);
-	return true;
-}
+// 		if (bind(serv_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+// 			throw 3;
 
-// hbchecker::sendHB
-// Description - Heartbeat 메시지 미도착시 사용(송신 주소, 송신할 메시지)
-// return - True or False
-bool hbchecker::sendHB(char* hb_addr, char* send_msg)
-{
-	int send_fd;
-	try {
-		send_fd = socket(AF_INET, SOCK_DGRAM, 0);
-		if ( send_fd < 0 )
-			throw send_fd;
+// 		cout << "[=] HB function Activated" << endl;
 
-		struct sockaddr_in send_addr;
-		memset( &send_addr, 0, sizeof(send_addr) );
-		send_addr.sin_family		= AF_INET;
-		send_addr.sin_port		= htons(HB_PORT);
-		send_addr.sin_addr.s_addr	= inet_addr(hb_addr);
+// 	} 
+// 	catch(int except){
+// 		cout << (except == 1? "Sock":"Bind")  << "erorr occured!" << endl;
+// 		return;
+// 	}
+// }
 
-		sendto(send_fd, send_msg, strlen(send_msg) + 1, 0, (struct sockaddr*)&send_addr, sizeof(send_addr));
-	} catch(int except) {
-		cout << "[!] Send error!" << endl;
-		close( send_fd );
-		return false;
-	}
+// // hbchecker::getOneTimeKey
+// // Description - OneTimeKey를 출력
+// // return - (char*)OneTimeKey
+// char* hbchecker::getOneTimeKey() {
+// 	return OneTimeKey;
+// }
 
-	cout << "[=] Send Complete! Wait for HB Signal.." << endl;
+// // hbchecker::getTimestamp
+// // Description - timestamp
+// // return - (time_t)timestamp
+// time_t hbchecker::getTimestamp() {
+// 	return timestamp;
+// }
 
-	close( send_fd );	
-	return true;
-}
+
+// // hbchecker::recvHB
+// // Description - Heartbeat 수신부 생성
+// // return - True or False
+// bool hbchecker::recvHB()
+// {
+// 	if (serv_fd < 1)
+// 		throw false;
+// 	try {
+// 		sockaddr_in send_addr;
+// 		int msglen;
+// 		int send_addrlen;
+// 	        int recv_err;
+// 		char buf[MAXSENDBUFSIZE]; 
+	
+// 		while(true) {
+// 			send_addrlen = sizeof(send_addr);
+// 			msglen = recvfrom(serv_fd, buf, MAXRECEIVEBUFSIZE, 0, (struct sockaddr*)&send_addr, (socklen_t*)&send_addrlen);
+// 			buf[msglen] = '\0';
+// 			cout << "[UDP/" << inet_ntoa(send_addr.sin_addr) << ":" << ntohs(send_addr.sin_port) << "]" << endl;
+// 		}
+// 	} catch(bool except) {
+// 		cout << "Initialization of the socket is needed!" << endl;
+// 		close(serv_fd);
+// 		return false;
+// 	}
+// 	close(serv_fd);
+// 	return true;
+// }
+
+// // hbchecker::sendHB
+// // Description - Heartbeat 메시지 미도착시 사용(송신 주소, 송신할 메시지)
+// // return - True or False
+// bool hbchecker::sendHB(char* hb_addr, char* send_msg)
+// {
+// 	int send_fd;
+// 	try {
+// 		send_fd = socket(AF_INET, SOCK_DGRAM, 0);
+// 		if ( send_fd < 0 )
+// 			throw send_fd;
+
+// 		struct sockaddr_in send_addr;
+// 		memset( &send_addr, 0, sizeof(send_addr) );
+// 		send_addr.sin_family		= AF_INET;
+// 		send_addr.sin_port		= htons(HB_PORT);
+// 		send_addr.sin_addr.s_addr	= inet_addr(hb_addr);
+
+// 		sendto(send_fd, send_msg, strlen(send_msg) + 1, 0, (struct sockaddr*)&send_addr, sizeof(send_addr));
+// 	} catch(int except) {
+// 		cout << "[!] Send error!" << endl;
+// 		close( send_fd );
+// 		return false;
+// 	}
+
+// 	cout << "[=] Send Complete! Wait for HB Signal.." << endl;
+
+// 	close( send_fd );	
+// 	return true;
+// }
--- source_code/hb_server.hh
+++ source_code/hb_server.hh
@@ -1,5 +1,6 @@
 #ifndef __HBCHECKER_HH__
 #define __HBCHECKER_HH__
+#include "util.hh"
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -7,43 +8,48 @@
 #include <iostream>
 #include <cstring>
 
-#define HB_PORT			60000
-#define BYTE			unsigned char
-#define BUFF_SIZE		1024
-#define MAXCONNECTIONS		5
-#define MAXSENDBUFSIZE		1023
-#define	MAXRECEIVEBUFSIZE	1024
-#define MAXWAITBUFSIZE		4096	
+// #define HB_PORT			60000
+// #define BYTE			unsigned char
+// #define BUFF_SIZE		1024
+// #define MAXCONNECTIONS		5
+// #define MAXSENDBUFSIZE		1023
+// #define	MAXRECEIVEBUFSIZE	1024
+// #define MAXWAITBUFSIZE		4096	
 
-#define	HBHDR_FLAG		0x04
-#define HBHDR_OTK		8
-#define HBHDR_TIME		4
-#define OTK_SIZE 4
+// #define	HBHDR_FLAG		0x04
+// #define HBHDR_OTK		8
+// #define HBHDR_TIME		4
+// #define OTK_SIZE 4
 
-struct HB_HDR
-{
-	unsigned char			flag;
-	unsigned char		 	otk[HBHDR_OTK];
-	unsigned int			timestamp;
+// struct HB_HDR
+// {
+// 	unsigned char			flag;
+// 	unsigned char		 	otk[HBHDR_OTK];
+// 	unsigned int			timestamp;
+// };
+
+
+// using namespace std;
+
+namespace hbchecker{
+  void setOneTimeKey(char* OTK);
+  void* hbCheckerMain(void* args);
 };
 
-
-using namespace std;
-
-class hbchecker {
-private:
-  sockaddr_in serv_addr;
-  char OneTimeKey[OTK_SIZE];
-  time_t timestamp;
-  int serv_fd;
-public:
-  hbchecker(char* _OneTimeKey, time_t _Timestamp);
-  char*		getOneTimeKey();
-  time_t	getTimestamp();
-  void		setTempinfo(char* _OneTimeKey, time_t _timestamp);
-  void		setOneTimeKey(char* _OneTimeKey);
-  void		setTimestamp(time_t _Timestamp);
-  bool 		recvHB();
-  bool 		sendHB(char* hb_addr, char* send_msg);
-};
+// class hbchecker {
+// private:
+//   sockaddr_in serv_addr;
+//   char OneTimeKey[OTK_SIZE];
+//   time_t timestamp;
+//   int serv_fd;
+// public:
+//   hbchecker(char* _OneTimeKey, time_t _Timestamp);
+//   char*		getOneTimeKey();
+//   time_t	getTimestamp();
+//   void		setTempinfo(char* _OneTimeKey, time_t _timestamp);
+//   void		setOneTimeKey(char* _OneTimeKey);
+//   void		setTimestamp(time_t _Timestamp);
+//   bool 		recvHB();
+//   bool 		sendHB(char* hb_addr, char* send_msg);
+// };
 #endif
