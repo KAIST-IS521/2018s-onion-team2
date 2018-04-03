ebc9751a0d7396758a62c0ffd4e4e1fc779e38b6
Taek Jin Lee <hexife@kaist.ac.kr>
2018-04-04 02:07:12 +0900

    Fix wrong shell script

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if [ !$1 ]; then
+if [ $# -eq 0 ]; then
   echo "Useage :: ./run.sh [ client name ] [ ssh port ]"
   exit
 fi
 
-if [!$2 && $1 -eq "server"]; then
+if [ $# -eq 1 ] && [ $1 == "server" ]; then
   docker run -id --net onionnet --ip 172.20.254.254 --name onion_$1 onionmess
-elif[!$2]; then
+elif [ $# -eq 1 ]; then
   docker run -id --net onionnet --name onion_$1 onionmess
-elif [ $1 -eq "server"]; then
+elif [ $1 == "server" ]; then
   docker run -id --net onionnet --ip 172.20.254.254 -p $2:22 --name onion_$1 onionmess
 else
-  docker run -id -net onionnet -p $2:22 --name onion_$1 onionmess
+  docker run -id --net onionnet -p $2:22 --name onion_$1 onionmess
 fi
 
 docker start onion_$1
