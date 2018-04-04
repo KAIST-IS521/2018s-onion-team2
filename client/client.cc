312ea41fd1668e342235e76d4a49f31400e6491f
Su Young Lee <sevendays37@kaist.ac.kr>
2018-04-05 01:41:28 +0900

    Adding an argument parser for the prototype

--- client/client.cc
+++ client/client.cc
@@ -1,3 +1,40 @@
-int main(int argc, char* argv[]){
+#include <iostream>
+#include <unistd.h>
+#include <string>
+using namespace std;
+
+void printHelp(char* const argv[]){
+  cout << "Usage: " + string(argv[0]) + " -p [PORT] -m [MESSAGE]" << endl;
+  exit(0);
+};
+
+int main(int argc, char* const argv[]){
+  int opt;
+  int port = -1;
+  string message = "";
+  
+  while((opt = getopt(argc, argv, "p:m:h")) != -1){
+    switch(opt){
+      case 'p':
+        // Set a port
+        port = atoi(optarg);
+        break;
+      case 'm':
+        // Set a message
+        message = optarg;
+        break;
+      case 'h':
+        // Print help message
+        printHelp(argv);
+        break;
+    }
+  }
+  
+  // Wrong use of arguments
+  if(port == -1 || message == "") {
+    printHelp(argv);
+    return 1;
+  }
+
   return 0;
 }
