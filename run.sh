8ed27a5138117b9b015580f9403b40888bd7bfee
Taek Jin Lee <hexife@kaist.ac.kr>
2018-04-09 23:10:57 +0900

    Append help in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
   echo "Useage :: ./run.sh [ client name ]"
+  echo "clinet name list -"
+  echo " donovan"
+  echo " marvin"
+  echo " sandra"
+  echo " stanton"
+  echo " jason"
   exit
 fi
 
